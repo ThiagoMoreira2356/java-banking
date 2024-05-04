bc46ba7bc611879ddbf96f2a1a5767d7a3add4fd
Thiago Moreira <thiago.moreira.job@gmail.com>
2024-05-04 17:53:11 -0300

    Criação de objeto conta002

--- src/conta/CriarConta.java
+++ src/conta/CriarConta.java
@@ -8,11 +8,17 @@ public class CriarConta {
         conta001.agencia = 291;
         conta001.numeroDaConta= 1;
 
+        Contas conta002 = new Contas();
+
         conta001.deposita(10);
         conta001.saque(1000);
-
         conta001.extrato();
 
+        conta002.nomeDoTitular = "Moreira Juridico";
+        conta002.numeroDaConta = 2;
+        conta002.agencia = 291;
+        conta002.saldo = 500;
 
+        conta002.extrato();
     }
 }
